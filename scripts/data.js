d66c2384f7ab5393509ec86e309a928ba516cd2b
Vgsj71 <vikramsinghv1970@gmail.com>
2025-02-13 02:39:31 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,129 @@
+const items = [
+    {
+        id:'001',
+        item_image:'images/1.jpg',
+        rating: {
+            stars: 4.5,
+            noOfReviews:1400,
+        },
+        company_name: 'Carlton London',
+        item_name: 'Rhodiun-Plated CZ Floral Studs',
+        current_price: 606,
+        original_price: 1045,
+        discount: 42,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'002',
+        item_image:'images/2.jpg',
+        rating: {
+            stars: 4.3,
+            noOfReviews:24,
+        },
+        company_name: 'CUKOO',
+        item_name: 'Women Padded Halter Neck Swimming Dress',
+        current_price: 1507,
+        original_price: 2599,
+        discount: 42,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'003',
+        item_image:'images/3.jpg',
+        rating: {
+            stars: 4.1,
+            noOfReviews:249,
+        },
+        company_name: 'NUEVOSDAMAS',
+        item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
+        current_price: 495,
+        original_price: 1599,
+        discount: 69,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'004',
+        item_image:'images/4.jpg',
+        rating: {
+            stars: 5.0,
+            noOfReviews:10,
+        },
+        company_name: 'Carlton London',
+        item_name: 'Rhodiun-Plated CZ Floral Studs',
+        current_price: 999,
+        original_price: 999,
+        discount: 0,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'005',
+        item_image:'images/5.jpg',
+        rating: {
+            stars: 4.2,
+            noOfReviews:3400,
+        },
+        company_name: 'Roadster',
+        item_name: 'Pure Cotton T-shirt',
+        current_price: 489,
+        original_price: 1399,
+        discount: 65,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'006',
+        item_image:'images/6.jpg',
+        rating: {
+            stars: 4.5,
+            noOfReviews:1400,
+        },
+        company_name: 'Nike',
+        item_name: 'Men ReactX Running Shoes',
+        current_price: 606,
+        original_price: 1045,
+        discount: 42,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'007',
+        item_image:'images/7.jpg',
+        rating: {
+            stars: 4.2,
+            noOfReviews:388,
+        },
+        company_name: 'The Indian Garage Co',
+        item_name: 'Men Slim Fit Regular Shorts',
+        current_price: 639,
+        original_price: 1599,
+        discount: 60,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    },
+
+    {
+        id:'008',
+        item_image:'images/8.jpg',
+        rating: {
+            stars: 4.3,
+            noOfReviews:5200,
+        },
+        company_name: 'Nivea',
+        item_name: 'Men Fresh Deodrant 150ml',
+        current_price: 142,
+        original_price: 285,
+        discount: 50,
+        return_peroid:14,
+        delivery_date:'10 Dec 2024'
+    }
+]
